82f14df51878b59d385db381cdb1a792cea4ffd8
WangQianao <2423114282@qq.com>
2022-11-30 16:34:43 +0800

    完成地图的绘制

--- js/GameObject.js
+++ js/GameObject.js
@@ -2,7 +2,7 @@ const GAME_OBJECTS = [];
 export class GameObject {
     constructor()
     {
-        GAME_OBJECTS.push(this);
+        GAME_OBJECTS.push(this);//将元素放入数组
         this.timedelta=0;
         this.has_called_start=false;
     }
@@ -23,7 +23,7 @@ export class GameObject {
     destroy()
     {
         this.on_destroy();
-        for (let i in GAME_OBJECTS)
+        for (let i in GAME_OBJECTS)//将元素从数组中移除
         {
             const obj = GAME_OBJECTS[i];
             if(obj === this)
@@ -34,12 +34,12 @@ export class GameObject {
         }
     }
 }
-let last_timestamp;
+let last_timestamp;//上一个时间戳
 const step = timestamp =>{
-
+    //timestamp为当前时间戳
     for(let obj of GAME_OBJECTS)
     {
-        if(!obj.has_called_start)
+        if(!obj.has_called_start)//如果该元素还没有执行start函数
         {
             obj.has_called_start=true;
             obj.start();
